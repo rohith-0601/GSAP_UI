ae109d0f7358a0f1e9bbb3a534904b5019794027
rohith-0601 <rohithperugu3@gmail.com>
2025-10-12 14:32:26 +0530

    working

--- animations/src/components/FigmaUI.jsx
+++ animations/src/components/FigmaUI.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FigmaUI = () => {
   return (
     <>
-    <div>working on hacktober and will work on backend soon</div>
+    <div>will work on backend soon</div>
     </>
     
   )
